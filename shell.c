2ffa61c1c5021877093384808272460281019cbd
QuinnMcHenry <quinn.mchenry@student.chaminade.edu>
2026-04-27 12:38:21 -0800

    working

--- shell.c
+++ shell.c
@@ -32,150 +32,272 @@ strcmp()
 strtok()
 */
 
-#define SH_TOK_BUFSIZE 64
-#define SH_TOK_DELIM " \t\r\n\a"
-
-
-int sh_execute(char **args) {
-    int i;
-    if (args[0] == NULL) {
-        return 1;
-    }
-
-    for (i = 0; i < sh_num_builtins(); i++) {
-        if (strcmp(args[0], builtin_str[i]) == 0) {
-            return (*builtin_func[i])(args);
-        }
-    }
-    return sh_launch(args);
-}
-
+// builtins declarations
 int sh_cd(char **args);
 int sh_help(char **args);
 int sh_exit(char **args);
+int sh_print(char **args);
 
-char *builtin_str[] = {"cd", "help", "exit"};
+//builtin commands strings
+char *builtin_str[] = {
+  "cd",
+  "help",
+  "exit",
+  "print"
+};
 
-int (*builtin_func[]) (char **) = {&sh_cd, &sh_help, &sh_exit};
+int (*builtin_func[]) (char **) = {
+  &sh_cd,
+  &sh_help,
+  &sh_exit,
+  &sh_print
+};
 
 int sh_num_builtins() {
-    return sizeof(builtin_str) / sizeof(char *);
+  return sizeof(builtin_str) / sizeof(char *);
 }
 
+/*
+  Builtin function implementations.
+*/
 
-int sh_cd(char **args) {
-    if (args[i] == NULL) {
-        fprintf(stderr, "sh: expected argument to \ "cd\ "\n");
+/**
+   @brief Builtin command: change directory.
+   @param args List of args.  args[0] is "cd".  args[1] is the directory.
+   @return Always returns 1, to continue executing.
+ */
+int sh_cd(char **args)
+{
+  if (args[1] == NULL) {
+    fprintf(stderr, "sh: expected argument to \"cd\"\n");
+  } else {
+    if (chdir(args[1]) != 0) {
+      perror("sh");
+    }
+  }
+  return 1;
+}
+
+int sh_print(char **args) {
+    printf("Current process: %d\n", getpid());
+    return 1;
+}
+
+/**
+   @brief Builtin command: print help.
+   @param args List of args.  Not examined.
+   @return Always returns 1, to continue executing.
+ */
+int sh_help(char **args)
+{
+  int i;
+  printf("Quinn and Max's Shell\n");
+  printf("Type program names and arguments, and hit enter.\n");
+  printf("The following are built in:\n");
+
+  for (i = 0; i < sh_num_builtins(); i++) {
+    printf("  %s\n", builtin_str[i]);
+  }
+
+  printf("Use the man command for information on other programs.\n");
+  return 1;
+}
+
+/**
+   @brief Builtin command: exit.
+   @param args List of args.  Not examined.
+   @return Always returns 0, to terminate execution.
+ */
+int sh_exit(char **args)
+{
+  return 0;
+}
+
+/**
+  @brief Launch a program and wait for it to terminate.
+  @param args Null terminated list of arguments (including program).
+  @return Always returns 1, to continue execution.
+ */
+int sh_launch(char **args)
+{
+  pid_t pid;
+  int status;
+
+  pid = fork();
+  if (pid == 0) {
+    // Child process
+    if (execvp(args[0], args) == -1) {
+      perror("sh");
+    }
+    exit(EXIT_FAILURE);
+  } else if (pid < 0) {
+    // Error forking
+    perror("sh");
+  } else {
+    // Parent process
+    do {
+      waitpid(pid, &status, WUNTRACED);
+    } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+  }
+
+  return 1;
+}
+
+/**
+   @brief Execute shell built-in or launch program.
+   @param args Null terminated list of arguments.
+   @return 1 if the shell should continue running, 0 if it should terminate
+ */
+int sh_execute(char **args)
+{
+  int i;
+
+  if (args[0] == NULL) {
+    // An empty command was entered.
+    return 1;
+  }
+
+  for (i = 0; i < sh_num_builtins(); i++) {
+    if (strcmp(args[0], builtin_str[i]) == 0) {
+      return (*builtin_func[i])(args);
+    }
+  }
+
+  return sh_launch(args);
+}
+
+/**
+   @brief Read a line of input from stdin.
+   @return The line from stdin.
+ */
+char *sh_read_line(void)
+{
+#ifdef SH_USE_STD_GETLINE
+  char *line = NULL;
+  ssize_t bufsize = 0; // have getline allocate a buffer for us
+  if (getline(&line, &bufsize, stdin) == -1) {
+    if (feof(stdin)) {
+      exit(EXIT_SUCCESS);  // We received an EOF
+    } else  {
+      perror("sh: getline\n");
+      exit(EXIT_FAILURE);
+    }
+  }
+  return line;
+#else
+#define SH_RL_BUFSIZE 1024
+  int bufsize = SH_RL_BUFSIZE;
+  int position = 0;
+  char *buffer = malloc(sizeof(char) * bufsize);
+  int c;
+
+  if (!buffer) {
+    fprintf(stderr, "sh: allocation error\n");
+    exit(EXIT_FAILURE);
+  }
+
+  while (1) {
+    // Read a character
+    c = getchar();
+
+    if (c == EOF) {
+      exit(EXIT_SUCCESS);
+    } else if (c == '\n') {
+      buffer[position] = '\0';
+      return buffer;
     } else {
-        if (chdir(args[1]) != 0) {
-            perror("sh");
-        }
+      buffer[position] = c;
     }
-    return 1;
-}
+    position++;
 
-int sh_help(char **args) {
-    int i;
-    printf("Quinn and Max's Shell\n");
-    printf("Type program name and arguments, and hit enter\n");
-    printf("The following are built in: \n");
-    for (i = 0; i < sh_num_builtins(); i++) {
-        printf("    %s\n", builtin_str[i]);
-    }
-    printf("Use the man command for information on other programs.\n");
-    return 1;
-}
-
-int sh_exit(char **args) {
-    return 0;
-}
-
-int sh_launch(char **args) {
-    pid_t pid, wpid;
-    int status;
-    
-    pid = fork();
-    if (pid == 0) {
-        if (execvp(args[0], args) == -1) {
-            perror("sh");
-        }
-        exit(EXIT_FAILURE);
-    } else if (pid < 0) {
-        perror("sh");
-    } else {
-        do {
-            wpid = waitpid(pid, &status, WUNTRACED);
-        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-    }
-    return 1;
-}
-
-char** sh_split_line(char *line) {
-    int bufsize = SH_TOK_BUFSIZE;
-    char **tokens = malloc(bufsize * sizeof(char*));
-    char *token;
-
-    if (!tokens) {
+    // If we have exceeded the buffer, reallocate.
+    if (position >= bufsize) {
+      bufsize += SH_RL_BUFSIZE;
+      buffer = realloc(buffer, bufsize);
+      if (!buffer) {
         fprintf(stderr, "sh: allocation error\n");
         exit(EXIT_FAILURE);
+      }
+    }
+  }
+#endif
+}
+
+#define SH_TOK_BUFSIZE 64
+#define SH_TOK_DELIM " \t\r\n\a"
+/**
+   @brief Split a line into tokens (very naively).
+   @param line The line.
+   @return Null-terminated array of tokens.
+ */
+char **sh_split_line(char *line)
+{
+  int bufsize = SH_TOK_BUFSIZE, position = 0;
+  char **tokens = malloc(bufsize * sizeof(char*));
+  char *token, **tokens_backup;
+
+  if (!tokens) {
+    fprintf(stderr, "sh: allocation error\n");
+    exit(EXIT_FAILURE);
+  }
+
+  token = strtok(line, SH_TOK_DELIM);
+  while (token != NULL) {
+    tokens[position] = token;
+    position++;
+
+    if (position >= bufsize) {
+      bufsize += SH_TOK_BUFSIZE;
+      tokens_backup = tokens;
+      tokens = realloc(tokens, bufsize * sizeof(char*));
+      if (!tokens) {
+		free(tokens_backup);
+        fprintf(stderr, "sh: allocation error\n");
+        exit(EXIT_FAILURE);
+      }
     }
 
-    token = strtok(line, SH_TOK_DELIM);
-    while (token != NULL) {
-        tokens[position] = token;
-        position++;
-    
-        if (position >= bufsize) {
-            budsize += SH_TOK_BUFSIZE;
-            tokens = realloc(tokens, bufsize * sizeof(char*));
-            if (!tokens) {
-                fprintf(stderr, "sh: reallocation error\n");
-                exit(EXIT_FAILURE);
-            }
-        }
-        token = strtok(NULL, SH_TOK_DELIM);
-    }
-    tokens[position] = NULL;
-    return tokens;
+    token = strtok(NULL, SH_TOK_DELIM);
+  }
+  tokens[position] = NULL;
+  return tokens;
 }
 
+/**
+   @brief Loop getting input and executing it.
+ */
+void sh_loop(void)
+{
+  char *line;
+  char **args;
+  int status;
 
+  do {
+    printf("?~ ");
+    line = sh_read_line();
+    args = sh_split_line(line);
+    status = sh_execute(args);
 
-char* sh_read_line(void) {
-    char* line = NULL;
-    ssize_t bufsize = 0; 
-    
-    if (getline(&line, &bufsize, stdin) == -1) {
-        if (feof(stdin)) {
-            exit(EXIT_SUCCESS);
-        } else {
-            perror("readline");
-            exit(EXIT_FAILURE);
-        }
-    }
-    return line;
+    free(line);
+    free(args);
+  } while (status);
 }
 
-void sh_loop(void) {
-    char* line;
-    char** args;
-    int status;
-    
-    do {
-        printf("|> ");
-        line = sh_read_line();
-        args = sh_split_line(line);
-        status = sh_execute(args);
-        
-        free(line);
-        free(args);
-    } while (status);
+/**
+   @brief Main entry point.
+   @param argc Argument count.
+   @param argv Argument vector.
+   @return status code
+ */
+int main(int argc, char **argv)
+{
+  // Load config files, if any.
+
+  // Run command loop.
+  sh_loop();
+
+  // Perform any shutdown/cleanup.
+
+  return EXIT_SUCCESS;
 }
 
-
-int main(int argc, char** argv) {
-    
-    sh_loop();
-
-    return EXIT_SUCESS;  
-}
